020190eaa0bd9ee222f0a1d2b87b1e95439171bc
belal <lmmeh.app.jo@gmail.com>
2024-10-22 10:58:11 +0300

    Initial commit with XCFramework

---
+++ Package.swift
@@ -0,0 +1,26 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MnghaDataSource",
+    platforms: [
+        .iOS(.v14) // Define the platform and version
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "MnghaDataSource",
+            targets: ["MnghaDataSource"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        // The binary target that includes the XCFramework
+        .binaryTarget(
+            name: "MnghaDataSource",
+            path: "./Frameworks/MnghaDataSource.xcframework" // Adjust the path to your XCFramework
+        )
+    ]
+)
